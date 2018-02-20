301ffdcc39f2e1dfefe24b6b83e4a7140e644b1f
denzilferreira <ferreiradenzil@gmail.com>
2018-02-20 14:53:43 +0200

    - Syncable

--- com.aware.plugin.google.fused_location/src/main/java/com/aware/plugin/google/fused_location/Plugin.java
+++ com.aware.plugin.google.fused_location/src/main/java/com/aware/plugin/google/fused_location/Plugin.java
@@ -128,7 +128,7 @@ public class Plugin extends Aware_Plugin implements GoogleApiClient.ConnectionCa
 
             checkGeofences();
 
-            if (!Aware.isSyncEnabled(this, Provider.getAuthority(this)) && Aware.isStudy(this)) {
+            if (Aware.isStudy(this)) {
                 Account aware_account = Aware.getAWAREAccount(getApplicationContext());
                 String authority = Provider.getAuthority(getApplicationContext());
                 long frequency = Long.parseLong(Aware.getSetting(this, Aware_Preferences.FREQUENCY_WEBSERVICE)) * 60;
@@ -136,7 +136,7 @@ public class Plugin extends Aware_Plugin implements GoogleApiClient.ConnectionCa
                 ContentResolver.setIsSyncable(aware_account, authority, 1);
                 ContentResolver.setSyncAutomatically(aware_account, authority, true);
                 SyncRequest request = new SyncRequest.Builder()
-                        .syncPeriodic(frequency, frequency/3)
+                        .syncPeriodic(frequency, frequency / 3)
                         .setSyncAdapter(aware_account, authority)
                         .setExtras(new Bundle()).build();
                 ContentResolver.requestSync(request);
@@ -145,7 +145,7 @@ public class Plugin extends Aware_Plugin implements GoogleApiClient.ConnectionCa
                 ContentResolver.setIsSyncable(aware_account, location_authority, 1);
                 ContentResolver.setSyncAutomatically(aware_account, location_authority, true);
                 SyncRequest request_local = new SyncRequest.Builder()
-                        .syncPeriodic(frequency, frequency/3)
+                        .syncPeriodic(frequency, frequency / 3)
                         .setSyncAdapter(aware_account, location_authority)
                         .setExtras(new Bundle()).build();
                 ContentResolver.requestSync(request_local);
@@ -253,21 +253,19 @@ public class Plugin extends Aware_Plugin implements GoogleApiClient.ConnectionCa
     public void onDestroy() {
         super.onDestroy();
 
-        if (Aware.isSyncEnabled(this, Provider.getAuthority(this))) {
-            ContentResolver.setSyncAutomatically(Aware.getAWAREAccount(this), Provider.getAuthority(this), false);
-            ContentResolver.removePeriodicSync(
-                    Aware.getAWAREAccount(this),
-                    Provider.getAuthority(this),
-                    Bundle.EMPTY
-            );
+        ContentResolver.setSyncAutomatically(Aware.getAWAREAccount(this), Provider.getAuthority(this), false);
+        ContentResolver.removePeriodicSync(
+                Aware.getAWAREAccount(this),
+                Provider.getAuthority(this),
+                Bundle.EMPTY
+        );
 
-            ContentResolver.setSyncAutomatically(Aware.getAWAREAccount(this), Locations_Provider.getAuthority(this), false);
-            ContentResolver.removePeriodicSync(
-                    Aware.getAWAREAccount(this),
-                    Locations_Provider.getAuthority(this),
-                    Bundle.EMPTY
-            );
-        }
+        ContentResolver.setSyncAutomatically(Aware.getAWAREAccount(this), Locations_Provider.getAuthority(this), false);
+        ContentResolver.removePeriodicSync(
+                Aware.getAWAREAccount(this),
+                Locations_Provider.getAuthority(this),
+                Bundle.EMPTY
+        );
 
         Aware.setSetting(this, Settings.STATUS_GOOGLE_FUSED_LOCATION, false);
 
